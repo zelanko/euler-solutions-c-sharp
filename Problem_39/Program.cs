674aad81cac06b15b62f6137c5eb8e79b421e0c7
Ari Zelanko <zelanko@yahoo.com>
2024-08-29 18:27:29 -0400

    Implement Problem 39

---
+++ Problem_39/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+const double MAX_PARIMETER = 1000;
+
+for (var p = 3; p <= MAX_PARIMETER; p += 1)
+{
+    var solutionCount = 0;
+    for (var a = 1; a < p - 1; a += 1)
+    {
+        for (var b = 1; b < a; b += 1)
+        {
+            var c = CSideLength(a, b);
+            
+            if (a + b + c > p)
+                break;
+            if (!double.IsInteger(c) || a + b + c != p)
+                continue;
+            
+
+            if (solutionCount == 0)
+                Console.Write($"[{p}]: ");
+
+            solutionCount += 1;
+            Console.Write($"({a} {b} {c}), "); 
+        }
+    }
+    if (solutionCount > 0) Console.WriteLine($"Count: {solutionCount}");
+}
+
+double CSideLength(double a, double b) => Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
---
+++ Problem_39/readme.md
@@ -0,0 +1,13 @@
+## Problem Text
+
+If *p* is the perimeter of a right angle triangle with integral length sides, *{a, b, c}*, there are exactly three solutions for *p = 120*.
+
+*{20,48,52}, {24,45,51}, {30,40,50}*
+
+For which value of *p &le; 1000*, is the number of solutions maximised?
+
+## Scratch
+
+a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup>
+
+sqrt(a<sup>2</sup> + b<sup>2</sup>) = c
