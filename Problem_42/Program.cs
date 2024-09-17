9f5cde77478ed9dc04eae01e244eeb9d255a1ef7
Ari Zelanko <zelanko@yahoo.com>
2024-09-17 11:43:40 -0400

    Problem 42 Solution

---
+++ Problem_42/Program.cs
@@ -0,0 +1,40 @@
+﻿var triangular = LoadTriangleNumbers();
+
+var characterMap = LoadCharacterMap();
+
+using var reader = new StreamReader("0042_words.txt");
+
+int countOfTriangles = 0;
+string line = reader.ReadToEnd();
+var words = line.Split(',');
+foreach (var quotedWord in words)
+{
+    var word = quotedWord.Substring(1, quotedWord.Length - 2);
+    int charCount = 0;
+    foreach (var c in word)
+        charCount += characterMap[c];
+    if(triangular.Contains(charCount)) countOfTriangles += 1;
+}
+
+Console.WriteLine($"Triangle words: {countOfTriangles}");
+
+static HashSet<int> LoadTriangleNumbers()
+{
+    var triangular = new HashSet<int>();
+    for (var n = 1; n <= 500; n++)
+    {
+        var tn = n * (n + 1) / 2;
+        triangular.Add(tn);
+    }
+    return triangular;
+}
+
+static Dictionary<char, int> LoadCharacterMap()
+{
+    var characterMap = new Dictionary<char, int>();
+    for (var c = 'A'; c <= 'Z'; c++)
+    {
+        characterMap.Add(c, c - 64);
+    }
+    return characterMap;
+}
