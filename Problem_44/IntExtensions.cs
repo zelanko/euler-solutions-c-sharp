80d0ff6628b6020df4b7476e71926b7053f27f41
Ari Zelanko <zelanko@yahoo.com>
2024-10-05 12:11:19 -0400

    Implement ToPentagonal and test first 10.

---
+++ Problem_44/IntExtensions.cs
@@ -0,0 +1,8 @@
+namespace Problem_44;
+
+public static class IntExtensions
+{
+    public static int ToPentagonal(this int n) => (n * (3 * n - 1)) / 2;
+
+    
+}
---
+++ Problem_44/Program.cs
@@ -0,0 +1,6 @@
+﻿using Problem_44;
+
+for (var n = 1; n < 11; n += 1)
+{
+    Console.Write($"{n.ToPentagonal()}, ");
+}
