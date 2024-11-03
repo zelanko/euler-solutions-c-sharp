1fc98fb7f037c44ca7bdf3a29859404681d8234f
Ari Zelanko <zelanko@yahoo.com>
2024-11-03 14:10:22 -0500

    Can read and write to/from a stream.

--- Problem_44/Program.cs
+++ Problem_44/Program.cs
@@ -1,7 +1,27 @@
 ﻿using Problem_44;
 
-for (var n = 1; n < 11; n += 1)
+const string fileName = "TheFileIWrite";
+
+using var stream = File.OpenWrite(fileName);
+using var writer = new BinaryWriter(stream);
+
+for (var n = 1; n < 13; n += 1)
 {
-    Console.Write($"{n.ToPentagonal()}, ");
+    var p = n.ToPentagonal();
+    writer.Write(p);
 }
+writer.Close();
+stream.Close();
 Console.WriteLine();
+using var inStream = File.OpenRead(fileName);
+using var reader = new BinaryReader(inStream);
+
+int p1;
+while(inStream.Position < inStream.Length)
+{
+    p1 = reader.ReadInt32();
+    Console.Write($"{p1}, ");
+}
+
+reader.Close();
+inStream.Close();
